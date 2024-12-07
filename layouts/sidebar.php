26af31b95ed25940f48cc2f0222e37a62fdf9045
Ruphasa <rfrizqifauzan@gmail.com>
2024-12-07 10:14:48 +0700

    Tolong Iconnya dong rek

--- layouts/sidebar.php
+++ layouts/sidebar.php
@@ -28,27 +28,59 @@
                 <!-- Add icons to the links using the .nav-icon class 
             with font-awesome or any other icon font library -->
                 <li class="nav-item">
-                    <a href="adminIndex.php?page=dashboard" class="nav-link">
+                    <a href="" class="nav-link">
                         <i class="bi bi-house nav-icon" style="font-size: 2rem; color:606C7D;"></i>
                         <p>Halaman Utama</p>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="adminIndex.php?page=pending" class="nav-link">
-                        <i class="bi bi-envelope" style="font-size: 2rem; color:606C7D"></i>
-                        <p>Pending</p>
+                    <a href="" class="nav-link">
+                        <i class="bi bi-house nav-icon" style="font-size: 2rem; color:606C7D;"></i>
+                        <p>Pesan</p>
                     </a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link">
+                        <i class="bi bi-house nav-icon" style="font-size: 2rem; color:606C7D;"></i>
+                        <p>Umum</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link">
+                        <i class="bi bi-house nav-icon" style="font-size: 2rem; color:606C7D;"></i>
+                        <p>Akademik</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link">
+                        <i class="bi bi-house nav-icon" style="font-size: 2rem; color:606C7D;"></i>
+                        <p>UKT</p>
+                    </a>
+                </li>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link">
+                        <i class="bi bi-envelope" style="font-size: 2rem; color:606C7D"></i>
+                        <p>Surat & Kuisoneir</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link">
+                        <i class="bi bi-house nav-icon" style="font-size: 2rem; color:606C7D;"></i>
+                        <p>Tingkat Akhir</p>
+                    </a>
+                </li>
                 </li>
                 <li class="nav-item">
                     <a href="index.php?page=pelanggaranmu" class="nav-link">
                         <i class="bi bi-exclamation-circle" style="font-size: 2rem; color:606C7D"></i>
-                        <p>Verifikasi</p>
+                        <p>Pelanggaran</p>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a href="index.php?page=lapor" class="nav-link">
                         <i class="bi bi-clock" style="font-size: 2rem; color:606C7D"></i>
-                        <p>Riwayat</p>
+                        <p>Laporkan</p>
                     </a>
                 </li>
                 <li class="nav-item border">
--- pages/lapor.php
+++ pages/lapor.php
@@ -31,135 +31,139 @@
                 <h3 class="card-title">Form Lapor</h3>
             </div>
             <div class="card-body"">
-                <section class="content">
-                    <div class="container-fluid">
-                        <div class="row">
-                            <!-- left column -->
-                            <div class="col-md-6">
-                                <!-- general form elements -->
-                                <div class="card card-primary">
-                                    <!-- /.card-header -->
-                                    <!-- form start -->
-                                    <form>
-                                        <div class="card-body">
-                                            <div class="form-group">
-                                                <label for="exampleInputEmail1">Email address</label>
-                                                <input type="email" class="form-control" id="exampleInputEmail1"
-                                                    placeholder="Enter email">
-                                            </div>
-                                            <div class="form-group">
-                                                <label for="exampleInputPassword1">Password</label>
-                                                <input type="password" class="form-control" id="exampleInputPassword1"
-                                                    placeholder="Password">
-                                            </div>
-                                            <div class="form-group">
-                                                <label for="exampleInputFile">File input</label>
-                                                <div class="input-group">
-                                                    <div class="custom-file">
-                                                        <input type="file" class="custom-file-input"
-                                                            id="exampleInputFile">
-                                                        <label class="custom-file-label" for="exampleInputFile">Choose
-                                                            file</label>
-                                                    </div>
-                                                    <div class="input-group-append">
-                                                        <span class="input-group-text">Upload</span>
-                                                    </div>
+                <section class=" content">
+                <div class="container-fluid">
+                    <div class="row">
+                        <!-- left column -->
+                        <div class="col">
+                            <!-- general form elements -->
+                            <div class="card card-primary">
+                                <!-- /.card-header -->
+                                <!-- form start -->
+                                <form>
+                                    <div class="card-body">
+                                        <div class="form-group">
+                                            <label for="NIM">Nomor Induk</label>
+                                            <input type="text" class="form-control" id="NIM"
+                                                placeholder="Enter NIM">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="nama">Nama</label>
+                                            <input type="text" class="form-control" id="nama"
+                                                placeholder="Enter Full Name">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="nama">aturan yang dilanggar</label>
+                                            <select class="form-control" name="" id="">
+                                                <option value="0">Pilih Aturan</option>
+                                                <option value="1">Aturan 1</option>
+                                            </select>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="exampleInputFile">File input</label>
+                                            <div class="input-group">
+                                                <div class="custom-file">
+                                                    <input type="file" class="custom-file-input" id="exampleInputFile">
+                                                    <label class="custom-file-label" for="exampleInputFile">Choose
+                                                        file</label>
+                                                </div>
+                                                <div class="input-group-append">
+                                                    <span class="input-group-text">Upload</span>
                                                 </div>
                                             </div>
-                                            <div class="form-check">
-                                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                                                <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                                            </div>
                                         </div>
-                                        <!-- /.card-body -->
+                                    </div>
+                                    <!-- /.card-body -->
 
-                                        <div class="card-footer">
-                                            <button type="submit" class="btn btn-primary">Lapor</button>
-                                        </div>
-                                    </form>
-                                </div>
+                                    <div class="card-footer">
+                                        <button type="submit" class="btn btn-primary">Lapor</button>
+                                    </div>
+                                </form>
                             </div>
-                            <!-- /.card-body -->
-                            </form>
                         </div>
+                        <!-- /.card-body -->
                     </div>
-                </section>
+                </div>
+    </section>
+    </div>
+    </div>
+    </section>
 
-                <script>
-                    function tambahData() {
-                        $('#form-data').modal('show');
-                        $('#form-tambah').attr('action', 'action/bukuAction.php?act=save');
-                        $('#kategori_id').val('');
-                        $('#buku_kode').val('');
-                        $('#buku_nama').val('');
-                        $('#jumlah').val('');
-                        $('#deskripsi').val('');
-                        $('#gambar').val('');
+    <script>
+        function tambahData() {
+            $('#form-data').modal('show');
+            $('#form-tambah').attr('action', 'action/bukuAction.php?act=save');
+            $('#kategori_id').val('');
+            $('#buku_kode').val('');
+            $('#buku_nama').val('');
+            $('#jumlah').val('');
+            $('#deskripsi').val('');
+            $('#gambar').val('');
+        }
+
+        var tabelData;
+        $(document).ready(function () {
+            tabelData = $('#table-data').DataTable({
+                ajax: 'action/bukuAction.php?act=load',
+            });
+
+            $('#form-tambah').validate({
+                rules: {
+                    kategori_id: {
+                        required: true,
+                        minlength: 1
+                    },
+                    buku_kode: {
+                        required: true,
+                        minlength: 1
+                    },
+                    buku_nama: {
+                        required: true,
+                        minlength: 1
+                    },
+                    jumlah: {
+                        required: true,
+                        minlength: 1
+                    },
+                    deskripsi: {
+                        required: true,
+                        minlength: 10
+                    },
+                    gambar: {
+                        required: true,
+                        minlength: 10
                     }
-
-                    var tabelData;
-                    $(document).ready(function () {
-                        tabelData = $('#table-data').DataTable({
-                            ajax: 'action/bukuAction.php?act=load',
-                        });
-
-                        $('#form-tambah').validate({
-                            rules: {
-                                kategori_id: {
-                                    required: true,
-                                    minlength: 1
-                                },
-                                buku_kode: {
-                                    required: true,
-                                    minlength: 1
-                                },
-                                buku_nama: {
-                                    required: true,
-                                    minlength: 1
-                                },
-                                jumlah: {
-                                    required: true,
-                                    minlength: 1
-                                },
-                                deskripsi: {
-                                    required: true,
-                                    minlength: 10
-                                },
-                                gambar: {
-                                    required: true,
-                                    minlength: 10
-                                }
-                            },
-                            errorElement: 'span',
-                            errorPlacement: function (error, element) {
-                                error.addClass('invalid-feedback');
-                                element.closest('.form-group').append(error);
-                            },
-                            highlight: function (element, errorClass, validClass) {
-                                $(element).addClass('is-invalid');
-                            },
-                            unhighlight: function (element, errorClass, validClass) {
-                                $(element).removeClass('is-invalid');
-                            },
-                            submitHandler: function (form) {
-                                $.ajax({
-                                    url: $(form).attr('action'),
-                                    method: 'post',
-                                    data: $(form).serialize(),
-                                    success: function (response) {
-                                        var result = JSON.parse(response);
-                                        if (result.status) {
-                                            $('#form-data').modal('hide');
-                                            tabelData.ajax.reload(); // reload data tabel 
-                                        } else {
-                                            alert(result.message);
-                                        }
-                                    }
-                                });
+                },
+                errorElement: 'span',
+                errorPlacement: function (error, element) {
+                    error.addClass('invalid-feedback');
+                    element.closest('.form-group').append(error);
+                },
+                highlight: function (element, errorClass, validClass) {
+                    $(element).addClass('is-invalid');
+                },
+                unhighlight: function (element, errorClass, validClass) {
+                    $(element).removeClass('is-invalid');
+                },
+                submitHandler: function (form) {
+                    $.ajax({
+                        url: $(form).attr('action'),
+                        method: 'post',
+                        data: $(form).serialize(),
+                        success: function (response) {
+                            var result = JSON.parse(response);
+                            if (result.status) {
+                                $('#form-data').modal('hide');
+                                tabelData.ajax.reload(); // reload data tabel 
+                            } else {
+                                alert(result.message);
                             }
-                        });
-                    }); 
-                </script>
+                        }
+                    });
+                }
+            });
+        }); 
+    </script>
 </body>
 
 </html>
